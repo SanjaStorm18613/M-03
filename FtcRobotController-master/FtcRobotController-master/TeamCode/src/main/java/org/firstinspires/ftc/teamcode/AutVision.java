8cda0a1d55e65e246c4a3a800851718c27936419
enzomarcs <vieiraribeiroeduardo@gmail.com>
2023-01-05 17:52:28 -0300

    part 20 (5/1/23)

---
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutVision.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Subsystem.Braco;
+import org.firstinspires.ftc.teamcode.Subsystem.DTMecanum;
+import org.firstinspires.ftc.teamcode.Subsystem.Elevador;
+import org.firstinspires.ftc.teamcode.Subsystem.Garra;
+import org.firstinspires.ftc.teamcode.Subsystem.Yaw;
+import org.firstinspires.ftc.teamcode.Vision.VisionCtrl;
+
+@Autonomous(name = "AutVision", group = "Linear Opmode")
+public class AutVision extends LinearOpMode {
+
+    VisionCtrl vision;
+
+
+    public void runOpMode() {
+
+        vision = new VisionCtrl(this, hardwareMap, telemetry);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.update();
+        }
+
+        vision.stopStreaming();
+    }
+}
--- FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Pipeline.java
+++ FtcRobotController-master/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Pipeline.java
@@ -1,14 +1,73 @@
 package org.firstinspires.ftc.teamcode.Vision;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.util.ArrayList;
+
 public class Pipeline extends OpenCvPipeline {
+
+    Mat mat = new Mat(), mat2 = new Mat();
+    Mat result;
+    Telemetry telemetry;
+
+    public Pipeline(Telemetry t){
+        telemetry = t;
+    }
+
 
         @Override
         public Mat processFrame(Mat input) {
 
-            return input;
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_BGR2HLS);
+
+
+            Scalar lower = new Scalar (0, 0, 0);
+            Scalar upper = new Scalar (50, 255, 255);
+
+
+            Core.inRange(mat, lower, upper, mat);
+
+            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+
+            Imgproc.erode(mat, mat, kernel);
+            Imgproc.dilate(mat, mat, kernel);
+            Imgproc.GaussianBlur(mat, mat, new Size(3, 3), 10);
+            Imgproc.threshold(mat, mat, 20, 255, Imgproc.THRESH_BINARY);
+
+
+
+            ArrayList<MatOfPoint> contours = new ArrayList<>();
+
+            Mat temp = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+            Imgproc.findContours(mat, contours, temp, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+
+            if (result == null) result = new Mat();
+            else result.release();
+
+            telemetry.addData("height", mat.height());
+            telemetry.addData("width", mat.width());
+            telemetry.addData("input height", input.height());
+            telemetry.addData("input width", input.width());
+
+            Core.bitwise_and(input, input, result, mat);
+            mat.release();
+
+            return result;
+
         }
+
+        public void ell(Mat inp, int[] low, int[] hig){
+
+
+
+        }
+
 }
 
