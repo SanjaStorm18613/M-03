7f669578b7d9bf64ce6bfdb0a796ce430557b603
EduRiVi <vieiraribeiroeduardo@gmail.com>
2023-07-14 23:19:29 -0300

    part 57 (14/7/23)
    Finalização pipeline do sistema base de tracking da junção

--- FtcRobotController-master 8.2/TeamCode/src/main/java/org/firstinspires/ftc/team18613/Vision/TrackingJunction.java
+++ FtcRobotController-master 8.2/TeamCode/src/main/java/org/firstinspires/ftc/team18613/Vision/TrackingJunction.java
@@ -10,28 +10,28 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class TrackingJunction extends OpenCvPipeline {
 
-    Mat processFrame, matTrash, box;
+    Mat processFrame, matTrash;
     Scalar lowFilter, highFilter;
     ArrayList<MatOfPoint> contours, boxCont;
     RotatedRect rect;
     MatOfPoint element;
-    double lastWidth = 0, width = 0;
+    Point[] box;
+    Point midPoint;
+    double lastWidth, width = 0;
 
     public TrackingJunction() {
 
         processFrame = new Mat();
-        lowFilter = new Scalar(100, 50, 30);
-        highFilter = new Scalar(120, 360, 360);
+        lowFilter = new Scalar(40, 40, 50);
+        highFilter = new Scalar(90, 180, 360);
 
         matTrash = new Mat();
-        box = new Mat();
+        box = new Point[4];
+        midPoint = new Point();
         contours = new ArrayList<>();
         boxCont = new ArrayList<>();
         rect = new RotatedRect();
@@ -40,15 +40,21 @@ public class TrackingJunction extends OpenCvPipeline {
     @Override
     public Mat processFrame(Mat originalFrame) {
 
-        Imgproc.cvtColor(originalFrame, processFrame, Imgproc.COLOR_BGR2HSV);
+        Imgproc.cvtColor(originalFrame, processFrame, Imgproc.COLOR_BGR2HLS);
         Core.inRange(processFrame, lowFilter, highFilter, processFrame);
 
         matTrash = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
         Imgproc.morphologyEx(processFrame, processFrame, Imgproc.MORPH_OPEN, matTrash);
         Imgproc.GaussianBlur(processFrame, processFrame, new Size(3,3), 10);
 
+        contours.clear();
         Imgproc.findContours(processFrame, contours, matTrash, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        matTrash.release();
 
+        originalFrame.copyTo(processFrame);
+
+        element = null;
+        lastWidth = 0;
         if (contours.size() > 0) {
             for (MatOfPoint cont : contours) {
                 rect = Imgproc.minAreaRect(new MatOfPoint2f(cont.toArray()));
@@ -63,10 +69,18 @@ public class TrackingJunction extends OpenCvPipeline {
 
             if (element != null) {
                 rect = Imgproc.minAreaRect(new MatOfPoint2f(element.toArray()));
-                Imgproc.boxPoints(rect, box);
-                //boxCont.add({new MatOfPoint(new Point(bo)));
-               //mgproc.drawContours(processFrame, Imgproc.poibox, 0, new Scalar(0,0,255), 2);
-                //boxCont.clear();
+                rect.points(box);
+                boxCont.clear();
+                boxCont.add(new MatOfPoint(box));
+                Imgproc.drawContours(processFrame, boxCont, 0, new Scalar(0,0,255), 2);
+
+                if (rect.size.width > rect.size.height) {
+                    midPoint = new Point((box[0].x + box[1].x)/2, (box[0].y + box[1].y)/2);
+                } else {
+                    midPoint = new Point((box[1].x + box[2].x)/2, (box[1].y + box[2].y)/2);
+                }
+
+                Imgproc.circle(processFrame, midPoint, 5, new Scalar(0, 255, 0), -1);
 
             }
         }
